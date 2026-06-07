ffd24e809885ed90a594be224733c0e926536c7c
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2026-06-07 08:30:44 +0530

    feat: refactor withAuth function to improve parameter handling and error responses

--- src/lib/mongodb/withAuth.ts
+++ src/lib/mongodb/withAuth.ts
@@ -3,29 +3,26 @@ import { auth } from "../auth/auth";
 import { NextResponse } from "next/server";
 import { connectDB } from "./mongodb";
 
-export function withAuth<
-  T extends (...args: any[]) => Promise<Response>
->(fn: T) {
-  return async (...args: Parameters<T>): Promise<Response> => {
+export function withAuth<T extends (...args: any[]) => Promise<Response>>(
+  fn: T,
+) {
+  return async (req: Request, ...rest: any): Promise<Response> => {
     try {
       const session = await auth.api.getSession({
         headers: await headers(),
       });
 
       if (!session?.user) {
-        return NextResponse.json(
-          { error: "Unauthorized" },
-          { status: 401 }
-        );
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
       }
 
       await connectDB();
 
-      return await fn(...args, session);
+      return await fn(req, session);
     } catch (error: any) {
       return NextResponse.json(
         { error: error?.message || "Internal Server Error" },
-        { status: error?.status || 500 }
+        { status: error?.status || 500 },
       );
     }
   };
